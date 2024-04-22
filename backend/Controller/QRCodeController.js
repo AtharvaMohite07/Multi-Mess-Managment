4e337f9953ff6c1ea6111ffbbe664b35bc5ea92b
Atharva41 <atharva.mohite20@vit.edu>
2024-04-22 17:41:32 +0530

    QR Validation Changes

--- backend/Controller/QRCodeController.js
+++ backend/Controller/QRCodeController.js
@@ -1,6 +1,7 @@
 import QRCode from '../Models/QRCode.js';
 import qrcode from 'qrcode';
 import CryptoJS from 'crypto-js';
+import { updateDailyEntry } from './DailyEntryController.js';
 
 // Encryption Key (replace with your secure key)
 import * as crypto from 'crypto';
@@ -60,13 +61,13 @@ export async function generateQRCode(userId, planId, validityDate, mealType) {
 
 export async function validateQRCode(req, res) {
     try {
-        const {code} = req.body;
+        const { code } = req.body;
 
         // Find the QR code in the database
-        const qrCode = await QRCode.findOne({code});
+        const qrCode = await QRCode.findOne({ code });
 
         if (!qrCode) {
-            return res.status(404).json({message: 'QR code not found.'});
+            return res.status(404).json({ message: 'QR code not found.' });
         }
         const qrCodeDataUrl = `data:image/png;base64,${code}`;
 
@@ -74,11 +75,11 @@ export async function validateQRCode(req, res) {
         const decryptedData = CryptoJS.AES.decrypt(qrCodeDataUrl, encryptionKey).toString(CryptoJS.enc.Utf8);
 
         // Parse the decrypted data
-        const {userId, planId} = JSON.parse(decryptedData);
+        const { userId, planId , mealType } = JSON.parse(decryptedData);
 
         // Check if the QR code is still valid
         if (qrCode.validityDate < Date.now()) {
-            return res.status(400).json({message: 'QR code expired.'});
+            return res.status(400).json({ message: 'QR code expired.' });
         }
 
         // Check if the QR code has exceeded usage limit (example: 2 uses per day)
@@ -86,25 +87,26 @@ export async function validateQRCode(req, res) {
         const todayEnd = new Date().setHours(23, 59, 59, 999);
         const todaysUsage = await QRCode.countDocuments({
             _id: qrCode._id,
-            usageCount: {$gt: 0}, // Check for at least one use
-            updatedAt: {$gte: todayStart, $lte: todayEnd}
+            usageCount: { $gt: 0 }, // Check for at least one use
+            updatedAt: { $gte: todayStart, $lte: todayEnd }
         });
 
-        if (todaysUsage >= 2) {
-            return res.status(400).json({message: 'QR code usage limit exceeded for today.'});
+        if (todaysUsage >= 1) {
+            return res.status(400).json({ message: 'QR code usage limit exceeded for today.' });
         }
 
         // Update the usage count and 'isUsed' flag
         qrCode.usageCount++;
         qrCode.isUsed = true;
         await qrCode.save();
+        await updateDailyEntry(userId, mealType, planId);
 
-        res.json({message: 'QR code validated successfully.'});
+        res.json({ message: 'QR code validated successfully.' });
     } catch (err) {
         console.error(err);
-        res.status(500).json({message: 'Internal server error.'});
+        res.status(500).json({ message: 'Internal server error.' });
     }
-};
+}
 
 export async function showQRCode(req, res) {
     return new Promise(async (resolve, reject) => {
